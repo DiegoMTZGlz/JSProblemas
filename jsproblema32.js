e8db322c52a9da4e07de91aea1537c64f146c54e
Diego Martinez <a353198@uach.mx>
2023-04-04 22:10:32 -0600

    Problema 32, imprimir la lista de 20 personas

---
+++ jsproblema32.js
@@ -0,0 +1,5 @@
+let nombres = ["Diego", "Antonio", "Marcos", "Ramón", "Judith", "Ana", "Jorge", "Fernanda", "Luis", "Mónica", "Ricardo", "Alejandra", "David", "Lorena", "Daniel", "Patricia", "Gabriela", "Fabián", "Sofía", "Adrián"];
+
+for (let i = 0; i < nombres.length; i++) {
+  console.log(nombres[i]);
+}
